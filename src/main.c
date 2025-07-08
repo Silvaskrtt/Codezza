0acfcb98eb125ed5fee3b64b8c012a9e7fecf632
Murilo Chagas <154563139+Silvaskrtt@users.noreply.github.com>
2025-07-08 09:25:51 -0300

    Criando o comando para abrir o db

--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
 // Código principal (ex: `crm_PIZZA.c`)
 #include <stdio.h>
 #include <sqlite3.h>
+
+int main() {
+    sqlite3 *db;
+    char *erro = 0;
+    int rc;
+
+    rc = sqlite3_open("CRM_PIZZA.db", &db);
+
+}
