7b0ec22c566d19091965b74225fe6d49969b095e
Murilo Chagas <154563139+Silvaskrtt@users.noreply.github.com>
2025-07-09 09:09:52 -0300

    testando o banco

--- src/main.c
+++ src/main.c
@@ -1,21 +1,51 @@
-// Código principal
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 #include <sqlite3.h>
 
-int main() {
+int callback(void *NotUsed, int argc, char **argv, char **colName) {
+    for (int i = 0; i < argc; i++) {
+        printf("%s: %s\n", colName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("--------------\n");
+    return 0;
+}
 
-    //Estabelencendo uma conexão com banco de dados
+int main() {
+    // Configura o terminal para usar UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+
+    // Estabelecendo conexão com o banco de dados
     sqlite3 *db;
     char *erro = 0;
     int rc;
 
-    // abrindo o banco de dados
     rc = sqlite3_open("C:/Users/Murilo/Documents/Projeto_CRM_PIZZA/database/CRM_PIZZA.db", &db);
-    // Verificando se a conexão deu certo
     if (rc) {
         fprintf(stderr, "Erro ao abrir o banco: %s\n", sqlite3_errmsg(db));
         return 1;
     } else {
-        printf("Conexao com banco de dados estabelecida com sucesso!\n");
+        printf("Conexão com banco de dados estabelecida com sucesso!\n\n");
     }
+
+    // Consulta de simples de Select
+
+    //const char *sql = "SELECT * FROM tbl_Cliente;";
+    const char *sql = 
+        "SELECT c.ID_Cliente_PK, c.Nome, c.Sobrenome, "
+        "t.Num_Telefone, "
+        "e.Rua, e.Num_Casa, e.Bairro, e.Cidade "
+        "FROM tbl_Cliente c "
+        "LEFT JOIN tbl_Telefone_Cliente t ON c.ID_Cliente_PK = t.ID_Cliente_FK "
+        "LEFT JOIN tbl_Endereco_Cliente e ON c.ID_Cliente_PK = e.ID_Cliente_FK "
+        "ORDER BY c.ID_Cliente_PK;";
+
+    rc = sqlite3_exec(db, sql, callback, 0, &erro);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Erro ao executar SELECT: %s\n", erro);
+        sqlite3_free(erro);
+    }
+
+    sqlite3_close(db);
+    return 0;
 }
