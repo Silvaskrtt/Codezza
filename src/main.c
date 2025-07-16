048bcc07bdbd700a3009e74f5a8693adfc40db96
Murilo Chagas <154563139+Silvaskrtt@users.noreply.github.com>
2025-07-16 18:59:11 -0300

    feat: adicionando a função para inserir morada do cliente

--- src/main.c
+++ src/main.c
@@ -48,10 +48,9 @@ char* cadastrarCliente(sqlite3 *db, const char *nome, const char *sobrenome) {
     }
 
     sqlite3_finalize(stmt);
-    return novoID;
 }
 
-void addTelefoneCliente(sqlite3 *db, const char *id_cliente_pk, const char *num_telefone) {
+void addTelefoneCliente(sqlite3 *db, const char *id_cliente_fk, const char *num_telefone) {
     sqlite3_stmt *stmt;
     char novoID[11];
 
@@ -80,7 +79,7 @@ void addTelefoneCliente(sqlite3 *db, const char *id_cliente_pk, const char *num_
     }
 
     sqlite3_bind_text(stmt, 1, novoID, -1, SQLITE_STATIC);
-    sqlite3_bind_text(stmt, 2, id_cliente_pk, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, id_cliente_fk, -1, SQLITE_STATIC);
     sqlite3_bind_text(stmt, 3, num_telefone, -1, SQLITE_STATIC);
 
     if (sqlite3_step(stmt) != SQLITE_DONE) {
@@ -92,12 +91,56 @@ void addTelefoneCliente(sqlite3 *db, const char *id_cliente_pk, const char *num_
     sqlite3_finalize(stmt);
 }
 
+char* addEnderecoCliente(sqlite3 *db, const char *rua, const char *num_casa, const char *bairro, const char *cidade, const char *id_cliente_fk) {
+    sqlite3_stmt *stmt;
+    char novoID[11];
+
+    const char *sqlUltimoID = "SELECT ID_Endereco_PK FROM tbl_Endereco_Cliente ORDER BY ID_Endereco_PK DESC LIMIT 1;";
+    if (sqlite3_prepare_v2(db, sqlUltimoID, -1, &stmt, NULL) != SQLITE_OK) {
+        printf("Erro ao preparar SELECT: %s\n", sqlite3_errmsg(db));
+        return NULL;
+}
+
+    int ultimoNumero = 0;
+    if (sqlite3_step(stmt) == SQLITE_ROW) {
+        const unsigned char *ultimoID = sqlite3_column_text(stmt, 0);
+        if (ultimoID && strlen((const char*)ultimoID) >= 6) {
+            ultimoNumero = atoi((const char *)ultimoID + 3);
+        }
+    }
+    sqlite3_finalize(stmt);
+
+    // Gerando novo ID
+    sprintf(novoID, "END%03d", ultimoNumero + 1);
+
+    const char *sqlInsert = "INSERT INTO tbl_Endereco_Cliente(ID_Endereco_PK, Rua, Num_Casa, Bairro, Cidade, ID_Cliente_FK) VALUES (?, ?, ?, ?, ?, ?)";
+    if (sqlite3_prepare16_v2(db, sqlInsert, -1, &stmt, NULL) != SQLITE_OK) {
+        printf("Erro ao preparar INSERT: %s\n", sqlite3_errmsg(db));
+        return NULL;
+    }
+
+    sqlite3_bind_text(stmt, 1, novoID, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, id_cliente_fk, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, rua, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 4, num_casa, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 5, bairro, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 6, cidade, -1, SQLITE_STATIC);
+
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        printf("Erro ao executar INSERT: %s\n", sqlite3_errmsg(db));
+    } else {
+        printf("Endereço cadastrado com ID: %s\n", novoID);
+    }
+    
+    sqlite3_finalize(stmt);
+}
+
 int main() {
     if (!conectar_banco("C:/Users/Murilo/Documents/Codezza/database/CODEZZA.db")) {
         return 1;
     }
 
-    char nome[100], sobrenome[100], telefone[12];
+    char nome[100], sobrenome[100], telefone[12], rua[50], num_casa[10], bairro[50], cidade[50];
 
     printf("Nome do cliente: ");
     fgets(nome, sizeof(nome), stdin);
@@ -111,10 +154,27 @@ int main() {
     fgets(telefone, sizeof(telefone), stdin);
     telefone[strcspn(telefone, "\n")] = '\0';
 
+    printf("Rua: ");
+    fgets(rua, sizeof(rua), stdin);
+    rua[strcspn(rua, "\n")] = '\0';
+
+    printf("Numero da Casa: ");
+    fgets(num_casa, sizeof(num_casa), stdin);
+    num_casa[strcspn(num_casa, "\n")] = '\0';
+
+    printf("Bairro: ");
+    fgets(bairro, sizeof(bairro), stdin);
+    bairro[strcspn(bairro, "\n")] = '\0';
+
+    printf("Cidade: ");
+    fgets(cidade, sizeof(cidade), stdin);
+    cidade[strcspn(cidade, "\n")] = '\0';
+
     char *id_cliente_pk = cadastrarCliente(db, nome, sobrenome);
 
     if (id_cliente_pk != NULL) {
         addTelefoneCliente(db, id_cliente_pk, telefone);
+        addEnderecoCliente(db, id_cliente_pk, rua, num_casa, bairro, cidade);
         free(id_cliente_pk);
     }
 
