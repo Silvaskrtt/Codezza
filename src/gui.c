144762ed23d1a37dcc3fdd889dfdc0251e6010f2
Murilo Chagas <154563139+Silvaskrtt@users.noreply.github.com>
2025-07-18 18:09:33 -0300

    test: apenas compilando novamente

--- src/gui.c
+++ src/gui.c
@@ -1,19 +1,18 @@
 #include <gtk/gtk.h>  // Inclusão da biblioteca GTK
 
-// Função para criar o pixbuf
-GdkPixbuf *create_pixbuf(const gchar * filename) {
-
+// Função para criar o pixbuf a partir de um arquivo de imagem
+GdkPixbuf *create_pixbuf(const gchar *filename) {
     GdkPixbuf *pixbuf;
     GError *error = NULL;
+
     pixbuf = gdk_pixbuf_new_from_file(filename, &error);
 
     if (!pixbuf) {
-        fprintf(stderr, "%s\n", error->message);
+        fprintf(stderr, "Erro ao carregar imagem: %s\n", error->message);
         g_error_free(error);
     }
 
     return pixbuf;
-
 }
 
 int main(int argc, char *argv[]) {
@@ -22,27 +21,22 @@ int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);  // Inicializa o GTK
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);  // Cria a janela principal
-    gtk_window_set_title(GTK_WINDOW(window), "Codezza");  // Define o título da janela
+    gtk_window_set_title(GTK_WINDOW(window), "Codezza");  // Define o título
     gtk_window_set_default_size(GTK_WINDOW(window), 1000, 720);  // Define tamanho padrão
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); // Centralizando a Janela
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); // Centraliza
 
-        // icone da janela
-        GdkPixbuf *icon = create_pixbuf("C:/Users/Murilo/Downloads/codezza.png");
-        if (icon) {
+    // Define o ícone da janela
+    GdkPixbuf *icon = create_pixbuf("C:/Users/Murilo/Downloads/codezza.png");
+    if (icon) {
         gtk_window_set_icon(GTK_WINDOW(window), icon);
-
-        gtk_widget_show(window);
-
-        g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-        g_object_unref(icon); // Libera a meria do icone
+        g_object_unref(icon);  // Libera a memória do ícone
     }
-    
-    // Conecta o evento de fechar janela ao encerramento do programa
+
+    // Conecta o evento de fechar a janela com a função gtk_main_quit
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    gtk_widget_show_all(window);  // Exibe todos os widgets da janela
-    gtk_main();  // Inicia o loop principal (aguarda eventos)
+    gtk_widget_show_all(window);  // Exibe a janela e seus elementos
+    gtk_main();  // Entra no loop principal do GTK
 
     return 0;
 }
