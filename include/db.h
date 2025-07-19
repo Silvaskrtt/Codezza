6cf6b4375201bd4ff8c0f047e1bd25a0b6045667
Murilo Chagas <154563139+Silvaskrtt@users.noreply.github.com>
2025-07-18 21:11:31 -0300

    test: adicionando struct para lidar melhor com banco de dados

--- include/db.h
+++ include/db.h
@@ -3,6 +3,13 @@
 
 #include <sqlite3.h>
 
+typedef struct{
+    char ID_Cliente_PK[11];
+    char Nome[100];
+    char Sobrenome[100];
+} Cliente;
+
+
 extern sqlite3 *db; // Torna a variável global acessível para outros arquivos
 
 int conectar_banco(const char *caminho);
